2db08af0b503912fc98a766908c75d2919b80696
piinnn.fish <piinnn.fish@gmail.com>
2023-05-15 22:46:01 +0800

    Add another page for emulate a home page that include the data.

--- app/src/main/java/com/example/hw2/MainActivity2.kt
+++ app/src/main/java/com/example/hw2/MainActivity2.kt
@@ -1,23 +1,26 @@
 package com.example.hw2
 
-import android.content.Intent
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.GridView
+import android.widget.SimpleAdapter
 import android.widget.TextView
 import okhttp3.*
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.json.JSONObject
 import java.io.IOException
 import java.util.*
+import kotlin.collections.HashMap
+import android.view.View
+import androidx.core.content.ContentProviderCompat.requireContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
-        //OkHttp調用
-        //Get api
         val client = OkHttpClient()
         val urlBuilder = "http://10.0.2.2/app/read.php".toHttpUrlOrNull()
             ?.newBuilder()
@@ -41,8 +44,12 @@ class MainActivity : AppCompatActivity() {
                     val dataArray = jsonObject.getJSONArray("data")
 
                     runOnUiThread {
-                        val textView = findViewById<TextView>(R.id.textView4)
-                        textView.text = ""
+                        val gridView = findViewById<GridView>(R.id.gridView)
+
+                        val grid = ArrayList<HashMap<String, Any>>()
+                        //val textView = findViewById<TextView>(R.id.textView6)
+                        //textView.text = ""
+                        val list: MutableList<String> = ArrayList()
                         for (i in 0 until dataArray.length()){
                             val chatObject = dataArray.getJSONObject(i)
                             val no = chatObject.getString("no")
@@ -50,64 +57,32 @@ class MainActivity : AppCompatActivity() {
                             val date = chatObject.getString("date")
                             val title = chatObject.getString("title")
                             val content = chatObject.getString("content")
-                            textView.append("${date} ${timestamp}\n")
-                            textView.append("${title} ${content}\n")
+                            /*textView.append("${date} ${timestamp}\n")
+                            textView.append("${title} ${content}\n")*/
+                            list.add("${date} ${timestamp} \n ${title} ${content}\n")
+
                         }
+
+                        for (i in list.indices){
+                            val map = HashMap<String, Any>()
+                            map["title"] = list[i]
+                            grid.add(map)
+                        }
+                        val fromData = arrayOf("title")
+                        val toData = intArrayOf(R.id.textView6)
+                        val simpleAdapter = SimpleAdapter(this@MainActivity2, grid, R.layout.grid_item, fromData, toData)
+                        gridView.adapter = simpleAdapter
+
                     }
                 }
                 else{
                     println("Request failed")
                     runOnUiThread {
-                        findViewById<TextView>(R.id.textView4).text = "資料錯誤"
+                        findViewById<TextView>(R.id.textView6).text = "資料錯誤"
                     }
                 }
             }
         })
-
-
-        //Post api
-        val send = findViewById<Button>(R.id.button)
-        val date = findViewById<TextView>(R.id.editTextTextPersonName)
-        val title = findViewById<TextView>(R.id.editTextTextPersonName2)
-        val content = findViewById<TextView>(R.id.editTextTextPersonName3)
-
-
-        send.setOnClickListener {
-            val postDate = date.text.toString()
-            println("postDate--------${postDate}")
-            val postTitle = title.text.toString()
-            println("postTitle-------${postTitle}")
-            val postContent = content.text.toString()
-            println("postContent-----${postContent}")
-
-            val clientPost = OkHttpClient()
-            val responseBody = FormBody.Builder()
-                .add("date", "${postDate}")
-                .add("title", "${postTitle}")
-                .add("content", "${postContent}")
-                .build()
-            val requestPost = Request.Builder()
-                .url("http://10.0.2.2/app/insert.php")
-                .post(responseBody)
-                .build()
-
-            clientPost.newCall(requestPost).enqueue(object : Callback{
-                override fun onFailure(call: Call, e: IOException) {
-                    e.printStackTrace()
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    val responseBody = response.body?.string()
-                    println("POST--------${responseBody}")
-                }
-            })
-            val intent = Intent()
-            intent.setClass(this, MainActivity2::class.java)
-            startActivity(intent)
-            startActivity(Intent(this,MainActivity2::class.java))
-
-
-        }
 
 
         val timer = Timer()
@@ -133,9 +108,14 @@ class MainActivity : AppCompatActivity() {
                             val jsonObject = JSONObject(responseBody)
                             val dataArray = jsonObject.getJSONArray("data")
 
+
                             runOnUiThread {
-                                val textView = findViewById<TextView>(R.id.textView4)
-                                textView.text = ""
+                                val gridView = findViewById<GridView>(R.id.gridView)
+
+                                val grid = ArrayList<HashMap<String, Any>>()
+                                //val textView = findViewById<TextView>(R.id.textView6)
+                                //textView.text = ""
+                                val list: MutableList<String> = ArrayList()
                                 for (i in 0 until dataArray.length()){
                                     val chatObject = dataArray.getJSONObject(i)
                                     val no = chatObject.getString("no")
@@ -143,15 +123,29 @@ class MainActivity : AppCompatActivity() {
                                     val date = chatObject.getString("date")
                                     val title = chatObject.getString("title")
                                     val content = chatObject.getString("content")
-                                    textView.append("${date} ${timestamp}\n")
-                                    textView.append("${title} ${content}\n")
+                                    /*textView.append("${date} ${timestamp}\n")
+                                    textView.append("${title} ${content}\n")*/
+                                    list.add("${date} ${timestamp}\n ${title} ${content}\n")
+
                                 }
+
+                                for (i in list.indices){
+                                    val map = HashMap<String, Any>()
+                                    map["title"] = list[i]
+                                    grid.add(map)
+                                }
+                                val fromData = arrayOf("title")
+
+                                val toData = intArrayOf(R.id.textView6)
+                                val simpleAdapter = SimpleAdapter(this@MainActivity2, grid, R.layout.grid_item, fromData, toData)
+                                gridView.adapter = simpleAdapter
+
                             }
                         }
                         else{
                             println("Request failed")
                             runOnUiThread {
-                                findViewById<TextView>(R.id.textView4).text = "資料錯誤"
+                                findViewById<TextView>(R.id.textView6).text = "資料錯誤"
                             }
                         }
                     }
@@ -159,6 +153,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
         timer.schedule(timerTask, 0, 5000)
+
+
+
 
 
     }
